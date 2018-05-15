85a21b4e9a643df8912832ab141411a60cb69c40
Nicholas S <blanketastronomer@users.noreply.github.com>
2018-05-14 17:30:16 -0700

    Initialize automated_installer module

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='automated_installer',
+    version='0.1.dev0',
+    description='An automated commandline installer for Windows and OSX.',
+    url="https://github.com/blanketastronomer/automated_installer",
+    author='blanketastronomer',
+    author_email='blanketastronomer@users.noreply.github.com',
+    license='MIT',
+    packages=['automated_installer'],
+    install_requires=[],
+    python_requires=">=3.6.5"
+)
