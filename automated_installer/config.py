2a8352eae4d9c8a922a6bf49a842e9551901fb8e
Nicholas S <blanketastronomer@users.noreply.github.com>
2018-05-14 19:17:24 -0700

    Set up default config_object data structure

--- automated_installer/config.py
+++ automated_installer/config.py
@@ -12,7 +12,28 @@ class Config(object):
     config_object = DotMap()
 
     def __init__(self, **kwargs):
-        pass
+        """
+        Class constructor.  Takes a variable number of arguments.
+
+        Args:
+            **main_directory (str): The top-level directory where the utility will execute.
+            **installer_root (str): The directory where all installers are kept.
+            **application_root (str): The directory where all system applications should be installed.
+            **application_32_root (str): (Windows only) The directory where all 32-bit applications should be installed.
+        """
+
+        if os.name == 'nt':
+            self.config_object.home_directory = os.path.expandvars('$USERPROFILE')
+        else:
+            self.config_object.home_directory = os.path.expandvars('$HOME')
+
+        arguments = ['main_directory', 'installer_root', 'application_root', 'application_32_root', 'installers']
+
+        for arg in arguments:
+            if arg == 'installers':
+                self.config_object[arg] = kwargs.get(arg, [])
+            else:
+                self.config_object[arg] = kwargs.get(arg, '')
 
     def load(self, path):
         pass
